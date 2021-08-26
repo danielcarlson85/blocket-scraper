8b49d6338d407c03c7ee85db50e20514a132ceec
dcarlsonhornlund <dcarlsonhornlund@SE10-PF1GF6EW>
2021-08-26 19:27:05 +0200

    Move strings and remove search term

--- blocket_scraper.py
+++ blocket_scraper.py
@@ -1,39 +1,31 @@
 import os.path
 from managers import page_manager
 import statistics
+import constants
 
-baseURL = "https://www.blocket.se/annonser/hela_sverige"
-imageURL = "https://www.blocket.se"
-search_word = "bil"
-prefix = "&page="
+searchURL = "https://www.blocket.se/annonser/hela_sverige?q=yamaha%204335"
 
-
-def check_if_file_exist_and_delete():
+def check_if_file_exist_and_delete(search_word):
     if os.path.isfile(search_word + ".txt"):
         os.remove(search_word + ".txt")
 
 
 def get_full_page_url(page):
-    return baseURL + search_word + prefix + str(page + 1)
-
-
-def get_full_web_page_url():
-    return baseURL + search_word
-
-def clear_list_from_empty_price(prod_list):
-
-    for idx, prod in enumerate(prod_list):
-        if not prod.price:
-            del prodList[idx]
-    return prodList
+    return searchURL + constants.prefix + str(page + 1)
 
 
 def main():
 
-    check_if_file_exist_and_delete()
-    web_page_url = get_full_web_page_url()
-    parsed_web_page = page_manager.get_web_page(web_page_url)
+    parsed_web_page = page_manager.get_web_page(searchURL)
+    search_word = page_manager.get_search_word(parsed_web_page)
+
+    check_if_file_exist_and_delete(search_word)
+
     total_number_of_pages = page_manager.get_total_pages(parsed_web_page)
+
+    if not total_number_of_pages:
+        print("No products found")
+        return
 
     print("Number of pages: " + str(total_number_of_pages))
 
@@ -43,26 +35,26 @@ def main():
         full_url = get_full_page_url(page)
         print(full_url)
         webpage = page_manager.get_web_page(full_url)
-        page_manager.get_all_products(webpage, search_word, imageURL)
+        page_manager.get_all_products(webpage, search_word)
 
     for price in page_manager.Products:
-        priceList.append(int(price.price))
+        price_list.append(int(price.price))
 
     sum = 0
     for page in page_manager.Products:
         sum += int(page.price)
 
-    lowest_price = min(priceList)
+    lowest_price = min(price_list)
     middle_price = int(sum/len(page_manager.Products))
-    median_price = statistics.median(priceList)
-    highest_price = max(priceList)
+    median_price = statistics.median(price_list)
+    highest_price = max(price_list)
 
     print("Lowest price: " + str(lowest_price))
     print("Middle price: " + str(middle_price))
     print("Median price: " + str(median_price))
     print("Highest price: " + str(highest_price))
 
-    print("Total products found: " + str(len(page_manager.Products)))
+    print("Total products found with prices: " + str(len(page_manager.Products)))
     print()
 
 
--- managers/file_manager.py
+++ managers/file_manager.py
@@ -1,3 +1,9 @@
+import sys
+
 def save_text_to_file(name, price, url, filename):
-    f = open(filename + ".csv", "a", encoding="utf-8")
-    f.write(name + "," + price + "," + url + "\n")
+    try:
+        f = open(filename + ".csv", "a", encoding="utf-8")
+        f.write(name + "," + price + "," + url + "\n")
+    except:
+        print("Cannot open file, check if it is already opend")
+        sys.exit()
