7d98d58e7f3381e7c122fdb299aec5075069f2c4
dcarlsonhornlund <dcarlsonhornlund@SE10-PF1GF6EW>
2021-08-27 12:44:47 +0200

    Refactore to file_manager update .gitignore

--- managers/page_manager.py
+++ managers/page_manager.py
@@ -1,23 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-import constants
+from constants.constants import base_url
 from models import product
 from managers import file_manager, string_manager
 
 Products = []
+
 
 def get_web_page(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     return soup
 
+
 def get_search_word(web_page):
     return web_page.find_all("h1", class_="bnGrQe")[0].text.split("\"")[1]
 
 
 def get_all_products(web_page, search_word):
-
     all_products = web_page.find_all("article", class_="geRkWZ")
 
     for x in all_products:
@@ -26,7 +27,7 @@ def get_all_products(web_page, search_word):
         price = x.find("div", class_="jVOeSj").text
         price = string_manager.remove_all_charachters(price)
         url = x.find("a", class_="evOAPG")["href"]
-        url = constants.base_url + url
+        url = base_url + url
 
         if price:
             Products.append(product.Product(name, price, url))
@@ -37,13 +38,11 @@ def get_all_products(web_page, search_word):
 
 def get_total_pages(web_page):
     pages = web_page.find_all("a", class_="dMZGCO")
-#    if not len(pages) or 1:
-#        return 1
-
+    #    if not len(pages) or 1:
+    #        return 1
 
     total_page_numbers = []
     for page in pages:
         page_number = (page.getText())
         total_page_numbers.append(page_number)
     return int(total_page_numbers[-3])
-
