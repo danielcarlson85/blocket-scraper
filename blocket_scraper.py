edecc85e02e0887af5ff83751ee33aee64720a3d
danielcarlsonhornlund <daniel.carlson.hornlund@boundtechnologies.com>
2021-09-02 23:09:09 +0200

    Small fix#

--- blocket_scraper.py
+++ blocket_scraper.py
@@ -11,35 +11,35 @@ search_url = "https://www.blocket.se/annonser/hela_sverige/fordon/motorcyklar/cu
 
 def start(url):
 
-    if url:
-        search_url = url
+    price_list = []
+    full_blocket_base_webpage = page_manager.get_web_page(url)
+    filename = page_manager.get_search_word(full_blocket_base_webpage, url)
+    saved_products = file_manager.load_products_from_file(filename)
+    found_products = page_manager.get_all_products(saved_products, full_blocket_base_webpage, url, filename)
 
-        price_list = []
-        full_blocket_base_webpage = page_manager.get_web_page(search_url)
-        filename = page_manager.get_search_word(full_blocket_base_webpage, search_url)
-        saved_products = file_manager.load_products_from_file(filename)
-        found_products = page_manager.get_all_products(saved_products, full_blocket_base_webpage, search_url, filename)
+    for price in found_products:
+        price_list.append(int(price.price))
 
-        for price in found_products:
-            price_list.append(int(price.price))
+    found_products.sort(key=lambda x: x.price)
 
-        found_products.sort(key=lambda x: x.price)
+    price_sum = 0
+    for page in found_products:
+        price_sum += int(page.price)
 
-        price_sum = 0
-        for page in found_products:
-            price_sum += int(page.price)
+    lowest_price = min(price_list)
+    middle_price = int(price_sum / len(found_products))
+    median_price = statistics.median(price_list)
+    highest_price = max(price_list)
 
-        lowest_price = min(price_list)
-        middle_price = int(price_sum / len(found_products))
-        median_price = statistics.median(price_list)
-        highest_price = max(price_list)
+    print("Lowest price: " + str(lowest_price) + " " + found_products[0].url)
+    print("Middle price: " + str(middle_price))
+    print("Median price: " + str(median_price))
+    print("Highest price: " + str(highest_price) + " " + found_products[len(found_products)-1].url)
 
-        print("Lowest price: " + str(lowest_price) + " " + found_products[0].url)
-        print("Middle price: " + str(middle_price))
-        print("Median price: " + str(median_price))
-        print("Highest price: " + str(highest_price) + " " + found_products[len(found_products)-1].url)
+    print("Total products with prices: " + str(len(found_products)))
+    print("To see all results open file in /results folder")
 
-        print("Total products with prices: " + str(len(found_products)))
-        print("To see all results open file in /results folder")
+    return found_products
 
-        return found_products
+if __name__ == '__main__':
+        start(search_url)
